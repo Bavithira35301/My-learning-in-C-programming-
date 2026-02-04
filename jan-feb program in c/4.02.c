b8f1a8cff696712494e3bb0be870cacb036bdbd4
Bavithira35301 <bavithiravanitha@gmail.com>
2026-02-04 11:33:27 +0530

    Day 4 completed

---
+++ jan-feb program in c/4.02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, Division;
+    printf("Enter two numbers separated by a space: ");
+    scanf("%d %d", &num1, &num2);
+    if(num2 != 0) {
+        Division = num1/num2;
+        printf("The division of %d by %d is : %d", num1, num2, Division);
+        return 0;
+    }
+}
---
+++ jan-feb program in python/4.02..py
@@ -0,0 +1,7 @@
+num1=int(input())
+num2=int(input())
+if num2==0:
+    print("Division by zero is not allowed")
+else:
+    print(f"The division of {num1} by {num2} is {num1/num2}")
+    
